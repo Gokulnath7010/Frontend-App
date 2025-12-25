2f76ffb3fda4486eed6a4f6b801959c2ca74d22b
Gokulnath7010 <msdgokulnath007@gmail.com>
2025-12-25 22:09:53 +0530

    Rename auth.css to Auth.css for consistency

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import api from "../api/axios";
-import "../styles/auth.css";
+import "../styles/Auth.css";
 
 export default function Login() {
   const navigate = useNavigate();
